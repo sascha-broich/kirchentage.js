2a167ac821d9db5f22ce6388ad1628aeb201615d
Sascha Broich <sascha.broich@online.de>
2019-11-30 10:53:53 +0100

    Wochentags-Kalender befüllen

--- kirchentage.js
+++ kirchentage.js
@@ -1,10 +1,10 @@
 function openTab(element) {
-    sections=document.getElementsByTagName("section");
-    for(i=0;i<sections.length;i++) {
+    let sections=document.getElementsByTagName("section");
+    for(let i=0;i<sections.length;i++) {
         sections[i].classList.remove("active");
     }
-    tabs=document.getElementById("tabs").children;
-    for(i=0;i<tabs.length;i++) {
+    let tabs=document.getElementById("tabs").children;
+    for(let i=0;i<tabs.length;i++) {
         tabs[i].classList.remove("active");
     }
     document.getElementById(element.id.substring(4)).classList.add("active");
@@ -21,4 +21,24 @@ function openTab(element) {
           element.firstChild.nodeValue="Gregorianisch";
       }
       return true;
+  }
+
+  function setDaysOfMonth() {
+    let year=parseInt(document.getElementById("wochentage-jahr").value,10);
+    let month=document.getElementById("wochentage-monat").selectedIndex;
+    let julian=document.getElementById("wochentage-toggle").value;
+    let buttons=document.getElementById("wochentage-kalender").getElementsByTagName("button");
+
+    let date=new Date(year,month,1);
+    let dow=date.getDay();
+    dow=(6+dow)%7;
+    date.setFullYear(year,month+1,0);
+    let last=date.getDate()+dow;
+
+    for(let i=0;i<buttons.length;i++) 
+        buttons[i].innerHTML=(i>=dow && i<last)?(""+(i+1-dow)):"";
+  }
+
+  window.onload=function () {
+    setDaysOfMonth();
   }
